4df3f05c834f302d12c923bf5aba9f9eb42a3243
Erica Moreli <ericammoreli@gmail.com>
2022-07-30 13:27:57 -0300

    queue_management - firebase configuration

---
+++ lib/src/queue/external/queue_firestore_datasource.dart
@@ -0,0 +1,9 @@
+import 'package:queue_management/src/queue/datasource/queue_datasource.dart';
+
+class QueueFirestoreDatasource implements IQueueDataSource {
+  @override
+  Stream<List<Map>> getAllQueues() {
+    // TODO: implement getAllQueues
+    throw UnimplementedError();
+  }
+}
