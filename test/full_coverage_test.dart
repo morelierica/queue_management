a567d4ac34832801f1a89715a519141eb60cb39c
Erica Moreli <ericammoreli@gmail.com>
2022-07-30 12:33:22 -0300

    queue_management - adding full coverage test

---
+++ test/full_coverage_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_import
+import 'package:queue_management/main.dart';
+import 'package:queue_management/src/queue/datasource/queue_datasource.dart';
+import 'package:queue_management/src/queue/domain/entities/queue_entity.dart';
+import 'package:queue_management/src/queue/domain/repositories/queue_repository.dart';
+import 'package:queue_management/src/queue/domain/usecases/get_all_queues.dart';
+import 'package:queue_management/src/queue/domain/value_objects/order.dart';
+import 'package:queue_management/src/queue/infra/adapters/json_to_order.dart';
+import 'package:queue_management/src/queue/infra/adapters/json_to_queue.dart';
+import 'package:queue_management/src/queue/infra/repositories/queue_repository.dart';
+
+void main() {}
